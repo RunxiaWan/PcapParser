01e7dd19df89142494ca0be9f79bf3252add7dfd
RunxiaWan <wanrunxia@aliyun.com>
2016-07-12 17:02:28 +0800

    Add working issue

--- doc.go
+++ doc.go
@@ -8,5 +8,8 @@ package main
 /*
 this work are focus on parsing a Pcap file that defragment the fragments it has and assemble the tcp package in it.
 Idealy, you can use ./PcapParser -in inputfile -out outputfile to parse a pcap file.
-This work is not done yet, more works are needed.
-*/
+Now the programme is runnable, you can use it to defragment IPV4 fragments and reassemble tcp segments.
+Working issue:
+1. IPV6 defragmentation
+2. Error handling
+3. Synchronizing issue to exit the programme*/
